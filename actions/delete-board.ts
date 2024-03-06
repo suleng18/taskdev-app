be16d2ce501bf2cc254973c6d88cb25d9cd6105b
Suleng18 <90331457+suleng18@users.noreply.github.com>
2024-03-06 23:13:12 +0700

    06-March-24

--- actions/delete-board.ts
+++
@@ -1,9 +0,0 @@
-import { db } from '@/lib/db';
-
-export async function deleteBoard(id: string) {
-  await db.board.delete({
-    where: {
-      id,
-    },
-  });
-}
--- app/(platform)/(dashboard)/board/[boardId]/layout.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -2,6 +2,28 @@ import { db } from '@/lib/db';
 import { auth } from '@clerk/nextjs';
 import { notFound, redirect } from 'next/navigation';
 import React from 'react';
+import { BoardNavbar } from './_components/board-navbar';
+
+export async function generateMetadata({ params }: { params: { boardId: string } }) {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    return {
+      title: 'Board',
+    };
+  }
+
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+
+  return {
+    title: board?.title || 'Board',
+  };
+}
 
 const BoardIdLayout = async ({ children, params }: { children: React.ReactNode; params: { boardId: string } }) => {
   const { orgId } = auth();
@@ -26,6 +48,8 @@ const BoardIdLayout = async ({ children, params }: { children: React.ReactNode; 
       className="relative h-full bg-no-repeat bg-cover bg-center"
       style={{ backgroundImage: `url(${board.imageFullUrl})` }}
     >
+      <BoardNavbar data={board} />
+      <div className="absolute inset-0 bg-black/10"></div>
       <main className="relative pt-28 h-full">{children}</main>
     </div>
   );
