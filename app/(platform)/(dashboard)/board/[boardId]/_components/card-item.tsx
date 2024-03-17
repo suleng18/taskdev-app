8316b897c59222d2180da90f1ceea985a8f8a7d9
Suleng18 <90331457+suleng18@users.noreply.github.com>
2024-03-17 23:06:07 +0700

    17-March-24

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -11,22 +11,22 @@ interface CardItemProps {
 }
 
 export const CardItem = ({ data, index }: CardItemProps) => {
-  // const cardModal = useCardModal();
+  const cardModal = useCardModal();
 
   return (
-    // <Draggable draggableId={data.id} index={index}>
-    //   {(provided) => (
-    <div
-      // {...provided.draggableProps}
-      // {...provided.dragHandleProps}
-      // ref={provided.innerRef}
-      role="button"
-      // onClick={() => cardModal.onOpen(data.id)}
-      className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
-    >
-      {data.title}
-    </div>
-    //   )}
-    // </Draggable>
+    <Draggable draggableId={data.id} index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          role="button"
+          onClick={() => cardModal.onOpen(data.id)}
+          className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
+        >
+          {data.title}
+        </div>
+      )}
+    </Draggable>
   );
 };
